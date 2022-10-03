dcd938bad1c2e0fed855f9f116558b5027eb5cd8
ExaChethan <chethan.j@exathought.com>
2022-10-03 19:29:07 +0530

    added token authentication

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,5 +1,6 @@
 from dataclasses import fields
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model,authenticate
+from django.utils.translation import ugettext_lazy as _
 
 from rest_framework import serializers
 
@@ -14,3 +15,29 @@ class UserSerializers(ModelSerializer):
     def create(self,validated_data):
         """create new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validated_data)
+
+
+class AuthenticateSerializer(serializers.Serializer):
+    """serializer for authentication object"""
+    email = serializers.CharField()
+    password = serializers.CharField(
+        style = {'input_type':'password'}
+    )
+
+
+    def validate(self,attrs):
+        """validate and authenticate user"""
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password
+        )
+        if not user:
+            msg=_('Unable to authenticate with provided creds')
+            raise serializers.ValidationError(msg, code='authentication')
+
+        attrs['user']=user
+        return attrs
--- app/user/views.py
+++ app/user/views.py
@@ -1,8 +1,19 @@
-from rest_framework import generics
+import imp
 
-from serializers import UserSerializers
+from django.shortcuts import render
+from app.user import serializers
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from serializers import UserSerializers,AuthenticateSerializer
 # Create your views here.
 
 class CreateUserView(generics.CreateApiView):
     """create a user in the system"""
     serializer_class = UserSerializers
+
+class CreateTokenView(ObtainAuthToken):
+    """create a new auth token"""
+    serializer_class = AuthenticateSerializer
+    renderer_class = api_settings.Default_Rendere_Classes
